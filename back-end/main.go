5ef17d4ab970e34289a95ce7ecd9e32aa70e68bb
Shoisob Ahasan <shoisob@vipra.tech>
2022-07-10 15:10:49 +0100

    feat(back-end): add main.go

--- back-end/main.go
+++ back-end/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
+	resp, err := http.Get("https://stoic-server.herokuapp.com/random")
 
-	fmt.Println("Hello World")
+	// fmt.Println(resp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Print(string(body)[0])
 
 }
